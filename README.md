01b6d0406cd8c96b71154bc2e1320b92c9ada4f9
Lacy Morrow <me@lacymorrow.com>
2014-06-07 15:40:03 -0400

    Workin pretty well

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-xspf-playlister 0.0.1
+xspf-playlister 0.1.0
 ===============
+
+> *It actually works pretty well!*
 
 Generate an XSPF playlist file for audio and video files using PHP.
 
@@ -8,9 +10,17 @@ Use it on the command line once or as a dead-simple way to keep a playlist on th
 ## Usage
 Place all of your media files into a folder called `media`. Copy `playlister.php` and the `getid3` directory to the same location as the media directory. Run `playlister.php` or open it in your browser to generate and print your XSPF file. That's it!
 
-Supports `mp3`, `wav`, and `ogg` audio and `mp4`, `webm`, and `ogv` video formats. If two files of different types share a filename, they will be interpreted as two sources for the same track. Tracks will be titled by their filename sans extension. Additional creator and album information can be provided by organizing your files into a media/creator/album/file hierarchy. An image may be associated with a track by giving it the same filename. To associate one image with an entire folder of tracks, give it the filename `artwork`. `artwork` images associate themselves to every sibling and child directory and may be placed anywhere in your media directory hierarchy, so an `artwork.jpg` in the `media` directory will act as a global image, filling in for every track that did not already have one provided.
+Supports `mp3`, `wav`, and `ogg` audio and `mp4`, `webm`, and `ogv` video formats. 
 
-By default, the [getid3](http://getid3.sourceforge.net/) library is used to scan `mp3` files and will automatically use the meta information associated with a track, rather than the menu directory hierarchy. In order to alleviate server load, a small caching mechanism is in place. By default the playlist is cached every hour. Open `playlister.php` and edit the settings between lines 20-40 to change the functionality or provide a different media directory.
+If two video files of different types share a filename, they will be interpreted as two sources for the same track. 
+
+Tracks will be titled by their filename sans extension. Additional creator and album information can be provided by organizing your files into a media/creator/album/file hierarchy. 
+
+An image may be associated with a track by giving it the same filename. To associate one image with an entire folder of tracks, give it the filename `artwork`. `artwork` images associate themselves to every sibling and child directory and may be placed anywhere in your media directory hierarchy, so an `artwork.jpg` in the `media` directory will act as a global image, filling in for every track that did not already have one provided.
+
+By default, the [getid3](http://getid3.sourceforge.net/) library is used to scan `mp3` files and will automatically use the meta information associated with a track, rather than the menu directory hierarchy. In order to alleviate server load, a small caching mechanism is in place. 
+
+By default the playlist is cached every hour. Open `playlister.php` and edit the settings between lines 20-40 to change the functionality or provide a different media directory.
 
 #### Command Line:
 
--- playlister.php
+++ playlister.php
@@ -3,7 +3,7 @@
 /**
  * This class reads a directory of media files and generates a
  * XML/XSPF formatted playlist
- * 
+ *
  * @author     lacymorrow
  * @website    www.lacymorrow.com
  * @copyright  Copyright (c) 2012 Lacy Morrow. All rights reserved.
@@ -57,7 +57,7 @@ $playArr = array();
 $imgArr = array();
 $gloArr = array();
 
-if($argc > 1){ $media = $argc[1]; }
+if($argc > 1){ $media = $argv[1]; }
 
 // use cached playlist if exists and valid
 date_default_timezone_set('UTC');
@@ -85,7 +85,7 @@ echo $playFile;
 #####################################
 
 #####################################
-/* 
+/*
  * generateXML
  * generates an XSPF/XML document to be processed
  */
@@ -117,47 +117,44 @@ function generateXML($trackArr){
 
 
 #####################################
-/* 
+/*
  * scanMedia
- * scans directory structure for media files 
+ * scans directory structure for media files
  * and generates a playlist array
  */
 #####################################
 
-function scanMedia( $path = '.', $id3, $level = 1, $dir = ''){ 
-	global $playArr, $imgArr, $gloArr, $creator, $album;
-    $ignore = array( 'cgi-bin', '.', '..' ); 
+function scanMedia( $path = '.', $id3, $level = 0, $dir = ''){
+	  global $playArr, $imgArr, $gloArr, $creator, $album;
     // Directories to ignore
-    $dh = @opendir( $path ); 
-    // Open the directory to the handle $dh 
-    while( false !== ( $file = readdir( $dh ) ) ){ 
-    // Loop through the directory 
-     
-        if( !in_array( $file, $ignore ) ){ 
-        // Check that this file is not to be ignored 
-            if( is_dir( "$path/$file" ) ){ 
-            	if( $level == 1 ){ 
+    $ignore = array( 'cgi-bin', '.', '..' );
+    // Open the directory to the handle $dh
+    $dh = @opendir( $path );
+    // Loop through the directory
+    while( false !== ( $file = readdir( $dh ) ) ){
+        // Check that this file is not to be ignored
+        if( !in_array( $file, $ignore ) ){
+            if( is_dir( "$path/$file" ) ){
+            	if( $level == 1 ){
             		$creator = $file; $album = '' ;
-            	} else if( $level == 2 ){ 
-            		$album = $file; 
-            	} else { $creator = ''; $album = ''; }
-				// Store images for Creator/Album
-	            scanMedia( "$path/$file",$id3, ($level+1), (($dir == '')?$path:$dir)); 
-                // Re-call this same function but on a new directory. 
-                // this is what makes function recursive. 
-             
-            } else { 
+            	} else if( $level == 2 ){
+            		$album = $file;
+            	}
+              // Re-call this same function but on a new directory.
+              // this is what makes function recursive.
+	            scanMedia( "$path/$file",$id3, ($level+1), (($dir == '')?$path:$dir));
+            } else {
             	$ext = pathinfo($file, PATHINFO_EXTENSION);
 				// generate file id - COULD BE MORE INTUITIVE (removes extension)
 				$filename = pathinfo($file, PATHINFO_FILENAME);
-				if(strtolower($filename) == 'artwork' && checkType($ext)){
-					if($level == 0){ 
-						$globalImg = "$path/$file"; 
-					} else { 
+				if(strtolower($filename) == 'artwork' && checkType($ext) != 'invalid'){
+					if($level == 0){
+						$globalImg = "$path/$file";
+					} else {
 						$gloArr[] = array('path' => $path, 'file' => $file);
 					}
 				}
-				
+
 				// if image file
 				$ct = checkType($ext);
 				if($ct == 'image'){
@@ -169,13 +166,14 @@ function scanMedia( $path = '.', $id3, $level = 1, $dir = ''){
 					// Initialize getID3 engine //
 					$getID3 = new getID3;
 					// Analyze file and store returned data in $id3Info
-					$id3Info = $getID3->analyze("$path/$filename.$ext"); //!!! Could need absolute path					
+					$id3Info = $getID3->analyze("$path/$filename.$ext"); //!!! Could need absolute path
 					/*
 					 Optional: copies data from all subarrays of [tags] into [comments] so
 					 metadata is all available in one location for all tag formats
 					 metainformation is always available under [tags] even if this is not called
 					*/
 					getid3_lib::CopyTagsToComments($id3Info);
+					//
 					$iDuration = (isset($id3Info['playtime_string'])) ? $id3Info['playtime_string'] : '';
 					if($id3 == true){
 						$iCreator = (isset($id3Info['comments_html']['artist'][0]) ? $id3Info['comments_html']['artist'][0] : $creator);
@@ -187,23 +185,24 @@ function scanMedia( $path = '.', $id3, $level = 1, $dir = ''){
 						$playArr[$filename] = array('filename' => $filename, 'type' => checkType($ext), 'creator' => $creator, 'album' => $album, 'title' => $filename, 'annotation' => '', 'duration' => '', 'location' => array($file), 'image' => '', 'info' => '', 'path' => $path);
 					}
 				} else if($ct == 'video') {
-					//IF VIDEO
 					if(!isset($playArr[$filename]['location']) || $playArr[$filename]['location'] === NULL){
+						// First source
 						$l = array($file);
 					} else {
+						// Additional Sources
 						$l = $playArr[$filename]['location'];
 						array_push($l,$file);
 					}
 					$playArr[$filename] = array('filename' => $filename, 'type' => checkType($ext), 'creator' => $creator, 'album' => $album, 'title' => $filename, 'annotation' => '', 'duration' => '', 'location' => $l, 'image' => '', 'info' => '', 'path' => $path);
 				}
-             
+
             }
-         
-        } 
-     
-    } 
-    closedir( $dh ); 
-    // Close the directory handle 
+
+        }
+
+    } // endwhile
+    // Close the directory handle
+    closedir( $dh );
 
     // Merge loose image array with associated tracks
     foreach($playArr as &$playVal){
@@ -222,19 +221,19 @@ function scanMedia( $path = '.', $id3, $level = 1, $dir = ''){
     	}
     	//echo $globalImg;
     	// Apply global image
-    	if($playVal['image'] == '' && $globalImg != ''){ $playVal['image'] = $globalImg; }
-    	
+    	if($playVal['image'] == '' && isset($globalImg)){ $playVal['image'] = $globalImg; }
+
     }
-    
+
     // Directory Scan Complete
 	if($dir == ''){
 		return $playArr;
 	}
-} 
+}
 
 
 #####################################
-/* 
+/*
  * checkType
  * checks for valid filetype
  */
@@ -254,6 +253,5 @@ function checkType($ext){
 		return 'invalid';
 	}
 }
-
 
 ?>
